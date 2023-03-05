69044d8392492ac7012614866695d6382813cfe4
Cesar Lozano <lozanorbis@gmail.com>
2023-03-05 17:20:30 -0400

    YouTube V-1.0.0
    Primer Proyecto - Programación Web

---
+++ js/hamburguer.js
@@ -0,0 +1,8 @@
+const openClose = document.getElementById("open-close");
+aside = document.getElementById("aside");
+asideBarMin = document.getElementById("sidebarMin");
+
+
+openClose.addEventListener("click",()=>{
+        aside.classList.toggle('desplegar');
+})
